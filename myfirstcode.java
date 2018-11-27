3219cac2f976c9ccbacf665ac9f3abde29586966
vishwasnm1 <vishwasnm1@gmail.com>
2018-11-27 19:20:15 +0530

    Commited from IntelliJ Idea

--- myfirstcode.java
+++ myfirstcode.java
@@ -8,6 +8,7 @@ Class first
    System.out.println("The sum of the given numbers is:",c);
    System.out.println("This is the better code");
    System.out.println("better code");
+   System.out.println("Common sense illa kano");
 
   }
  }
