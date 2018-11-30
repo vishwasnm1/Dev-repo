0c09bb4b4cbb084fef27a81aeacaa90a691a3df2
vishwasnm1 <38805338+vishwasnm1@users.noreply.github.com>
2018-11-30 18:48:35 +0530

    added a html tag

--- myfirstcode.java
+++ myfirstcode.java
@@ -8,6 +8,8 @@ Class first
    System.out.println("The sum of the given numbers is:",c);
    System.out.println("This is the better code");
    System.out.println("This is the better code");
+   System.out.println("This is the better code");
+   <html></html>
   }
  
  }
