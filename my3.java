8b8a00f62eb84e25e621047c746eb3c90f1b07bc
vishwasnm1 <38805338+vishwasnm1@users.noreply.github.com>
2018-11-30 14:46:03 +0530

    added a SOP

--- my3.java
+++ my3.java
@@ -2,14 +2,10 @@ class three
 {
 	public void main() {
 		System.out.println("HI welcome to DevTools");
-subbranch3
 		System.out.println("Hello");
-
 		System.out.println("Hello All");
 		System.out.println("ALL");
-
-
- master
+                Ssytem.out.println("Hey");		
 	}
 }
 
