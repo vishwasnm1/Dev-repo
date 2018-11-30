4e5b2f1c120096d8cc13b16ffaa32b6b62e2948e
vishwasnm1 <38805338+vishwasnm1@users.noreply.github.com>
2018-11-30 14:48:00 +0530

    Refined the code

--- my3.java
+++ my3.java
@@ -2,11 +2,10 @@ class three
 {
 	public void main() {
 		System.out.println("HI welcome to DevTools");
-
 		System.out.println("Hello");
 		System.out.println("Hello All");
 		System.out.println("ALL");
-    Sytem.out.println("Hey");		
+                System.out.println("Hey");		
 	}
 }
 
